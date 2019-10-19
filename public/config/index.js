368c33196c04ce50e654f4c3ac2315a8d019593a
wangchao <h0893558@163.com>
2019-10-19 23:32:07 +0800

    fix

--- public/config/index.js
+++ public/config/index.js
@@ -2,6 +2,8 @@ window.airportConfig = {
   api: {
     // baseUrl: "http://1616d788a5.iok.la:23275/api",
     baseUrl: "http://172.23.162.6:3000/api",
+    // baseUrl: "http://22385a17n3.51mypc.cn:32125/api",
+    // baseUrl: "http://47.92.200.142:9999/api",
     menuListUrl:"./mock/menu.json"
   },
   container: {
