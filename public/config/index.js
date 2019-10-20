ea24b1785f955ec0b66baac9806f64951a0653a0
wangchao <h0893558@163.com>
2019-10-20 18:34:06 +0800

    123

--- public/config/index.js
+++ public/config/index.js
@@ -1,8 +1,9 @@
 window.airportConfig = {
   api: {
+    baseUrl: "http://172.23.162.10:3000/api",
     // baseUrl: "http://1616d788a5.iok.la:23275/api",
-    baseUrl: "http://172.23.162.6:3000/api",
-    // baseUrl: "http://22385a17n3.51mypc.cn:32125/api",
+    // baseUrl: "http://172.23.162.6:3000/api",
+    // baseUrl: "http://22385a17n3.51mypc.cn:30972//api", // 6的外网
     // baseUrl: "http://47.92.200.142:9999/api",
     menuListUrl:"./mock/menu.json"
   },
